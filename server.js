2a6dd4dc286f08951ab63e4795c4843de69ef87c
JorgeAlexandresantosfilho <jorgedvl309@gmail.com>
2025-05-16 17:24:39 -0300

    alteração na porta de 3000 para 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
